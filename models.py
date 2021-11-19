a57cb722791f0135f21e088cc9c1485557ae7248
jr1os <dev.alirios@gmail.com>
2021-11-19 23:36:41 +0000

    :seedling: update models

--- models.py
+++ models.py
@@ -8,4 +8,15 @@ class Products(db.Base):
     name = Column(String, nullable=False)
     price = Column(Float)
 
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __repr__(self):
+        return f"Product ({self.name} : {self.price})"
+
+    def __str__(self):
+        return self.name
+
     
