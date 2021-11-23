1763a4b4637e2a52e9e86651bbb9ead1d0d6f246
jr1os <dev.alirios@gmail.com>
2021-11-23 22:43:01 +0000

    upgrade doctest

--- leetcode/codeTwoSum.py
+++ leetcode/codeTwoSum.py
@@ -2,6 +2,14 @@ from typing import List
 
 
 def twoSum(nums: List[int], target: int) -> List[int]:
+    """
+    >>> twoSum([2, 7, 11, 15], 9)
+    [0, 1]
+    >>> twoSum([3, 2, 4], 6)
+    [1, 2]
+    >>> twoSum([3, 3], 6)
+    [0, 1]
+    """
     seen = {}
     for index, num in enumerate(nums):
         if target - num in seen:
